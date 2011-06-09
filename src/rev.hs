58e9d2f62a811a266b9e59fbda7a97ad977efe8c
Troels Henriksen <athas@sigkill.dk>
2011-06-09 22:46:17 +0200

    More better.

--- src/rev.hs
+++ src/rev.hs
@@ -2,49 +2,54 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE ExistentialQuantification #-}
-
-import Debug.Trace
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 import Control.Applicative
 import Control.Monad
 import Control.Monad.ST
 import Data.STRef
 import Data.Monoid as Monoid
-import Complex
 
+data RVal s a = RVal (STRef s a)
+              | Con a
+              | Oper (RIO s a)
 
-data RV s a = RV (STRef s a)
-            | Con a
+instance Functor (RVal s) where
+  f `fmap` x = Oper $ do x' <- peekV x
+                         return $ f x'
 
-con :: a -> RV s a
+instance Applicative (RVal s) where
+  pure = Con
+  f <*> b = Oper $ do f' <- peekV f
+                      b' <- peekV b
+                      return $ f' b'
+
+class RValue v s a where
+  value :: v s a -> RIO s a
+
+instance RValue RVal s a where
+  value = peekV
+
+con :: a -> RVal s a
 con = Con
 
-mkV :: a -> RIO s (RV s a)
-mkV v = RV <$> newSTRef v
+mkV :: a -> RIO s (RVal s a)
+mkV v = RVal <$> newSTRef v
 
-peekV :: RV s a -> RIO s a
-peekV (RV v) = readSTRef v
+peekV :: RVal s a -> RIO s a
+peekV (RVal v) = readSTRef v
 peekV (Con a) = return a
+peekV (Oper m) = m
 
-writeV :: RV s a -> a -> RIO s ()
-writeV (RV v) = writeSTRef v
+writeV :: RVal s a -> a -> RIO s ()
+writeV (RVal v) = writeSTRef v
 
-newtype RC s a = RC { unRC :: RIO s a } deriving (Functor)
-
-instance Applicative (RC s) where
-  pure = RC . return
-  RC f <*> RC b = RC $ f `ap` b
-
-val :: RV s a -> RC s a
-val = RC . peekV
-
--- QIO and U as traces
 data R s = RReturn
-         | forall a.Swap (RV s a) (RV s a) (R s)
-         | Cond (RC s Bool) (R s) (R s) (RC s Bool) (R s)
-         | Inc (RV s Integer) (RV s Integer) (R s)
-         | Dec (RV s Integer) (RV s Integer) (R s)
---         | Rlet Bool (Qbit -> R) R
+         | forall a.Swap (RVal s a) (RVal s a) (R s)
+         | Cond (RVal s Bool) (R s) (R s) (RVal s Bool) (R s)
+         | forall a b.Binop (a->b->a) (a->b->a) (RVal s a) (RVal s b) (R s)
+         | forall a.Rlet a (RVal s a -> R s) (R s)
 
 type RIO s = ST s
 
@@ -57,39 +62,47 @@ instance Monoid (R s) where
     mappend RReturn u = u
     mappend (Swap x y u) u' = Swap x y (mappend u u')
     mappend (Cond pre f1 f2 post u') u'' = Cond pre f1 f2 post (mappend u' u'')
-    mappend (Inc v1 v2 u') u'' = Inc v1 v2 (mappend u' u'')
-    mappend (Dec v1 v2 u') u'' = Dec v1 v2 (mappend u' u'')
---    mappend (Rlet b f u) u' = Rlet b f (mappend u u')
+    mappend (Binop f finv x y u') u'' = Binop f finv x y (mappend u' u'')
+    mappend (Rlet x f u) u' = Rlet x f (mappend u u')
 
 skip :: R s
 skip = RReturn
 
-swap :: (RV s a) -> (RV s a) -> R s
-swap x y = Swap x y RReturn
+infix 2 <=>
+(<=>) :: (RVal s a) -> (RVal s a) -> R s
+x <=> y = Swap x y RReturn
 
-cond :: RC s Bool -> R s -> R s -> RC s Bool -> R s
+cond :: RVal s Bool -> R s -> R s -> RVal s Bool -> R s
 cond pre f1 f2 post = Cond pre f1 f2 post RReturn
 
-inc :: RV s Integer -> RV s Integer -> R s
-inc v1 v2 = Inc v1 v2 RReturn
+binop :: (a->b->a) -> (a->b->a) -> RVal s a -> RVal s b -> R s
+binop f finv x y = Binop f finv x y RReturn
 
-dec :: RV s Integer -> RV s Integer -> R s
-dec v1 v2 = Dec v1 v2 RReturn
+infix 2 +=
+(+=) :: RVal s Integer -> RVal s Integer -> R s
+v1 += v2 = binop (+) (-) v1 v2
 
---ulet :: Bool -> (Qbit -> R) -> R
---ulet b ux = Rlet b ux RReturn
+infix 2 -=
+(-=) :: RVal s Integer -> RVal s Integer -> R s
+v1 -= v2 = binop (-) (+) v1 v2
 
-urev :: R s -> R s
-urev RReturn = RReturn
-urev (Swap x y u) = urev u `mappend` swap x y
-urev (Cond pre f1 f2 post u) = urev u `mappend` cond post (urev f1) (urev f2) pre
-urev (Inc v1 v2 u) = urev u `mappend` dec v1 v2
-urev (Dec v1 v2 u) = urev u `mappend` inc v1 v2
---urev (Rlet b xu u) = urev u `mappend` ulet b (urev.xu)
+infixr 1 !>
+(!>) :: R s -> R s -> R s
+(!>) = mappend
+
+rlet :: a -> (RVal s a -> R s) -> R s
+rlet x f = Rlet x f RReturn
+
+rev :: R s -> R s
+rev RReturn = RReturn
+rev (Swap x y u) = rev u !> x <=> y
+rev (Cond pre f1 f2 post u) = rev u !> cond post (rev f1) (rev f2) pre
+rev (Binop f finv x y u) = rev u !> binop finv f x y
+rev (Rlet x f u) = rev u !> rlet x (rev . f)
 
 applyR :: R s -> RIO s ()
 applyR RReturn = return ()
-applyR (Cond pre f1 f2 _ u) = do pre' <- unRC pre
+applyR (Cond pre f1 f2 _ u) = do pre' <- peekV pre
                                  applyR $ if pre' then f1 else f2
                                  applyR u
 applyR (Swap rx ry u) = do x <- peekV rx
@@ -97,22 +110,21 @@ applyR (Swap rx ry u) = do x <- peekV rx
                            writeV rx y
                            writeV ry x
                            applyR u
-applyR (Inc v1 v2 u) = do v1' <- peekV v1
-                          v2' <- peekV v2
-                          writeV v1 (v1'+v2')
-                          applyR u
-applyR (Dec v1 v2 u) = do v1' <- peekV v1
-                          v2' <- peekV v2
-                          writeV v1 (v1'-v2')
-                          applyR u
+applyR (Binop f _ v1 v2 u) = do writeV v1 =<< liftM2 f (peekV v1) (peekV v2)
+                                applyR u
+applyR (Rlet x f u) = do applyR =<< (f <$> mkV x)
+                         applyR u
 
-fibf :: RV s Integer -> RV s Integer -> RV s Integer -> R s
+fibf :: RVal s Integer -> RVal s Integer -> RVal s Integer -> R s
 fibf rn rx1 rx2 =
-  cond (pure (==0) <*> val rn)
-  (inc rx1 (con 1) `mappend` inc rx2 (con 1))
-  (dec rn (con 1) `mappend` fibf rn rx1 rx2
-   `mappend` inc rx1 rx2 `mappend` swap rx1 rx2)
-  (pure (==) <*> val rx1 <*> val rx2)
+  cond (liftA (==0) rn)
+  (rx1 += con 1 !>
+   rx2 += con 1)
+  (rn -= con 1 !>
+   fibf rn rx1 rx2 !>
+   rx1 += rx2 !>
+   rx1 <=> rx2)
+  (liftA2 (==) rx1 rx2)
 
 test :: Integer -> RIO s (Integer, Integer)
 test n = do
@@ -129,5 +141,5 @@ test2 x1 x2 = do
   rx1 <- mkV x1
   rx2 <- mkV x2
   rn <- mkV 0
-  applyR $ urev $ fibf rn rx1 rx2
+  applyR $ rev $ fibf rn rx1 rx2
   peekV rn
